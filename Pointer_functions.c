c06484becd44cf9574025ca7c38b14cdedf8ceb1
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-09 08:00:02 +0530

    Update Pointer_functions.c

--- Pointer_functions.c
+++ Pointer_functions.c
@@ -80,3 +80,54 @@ is not known for the time, and later it may be known. In the given code we
 are using a void pointer that is holding addresses of an integer and
 character type.*/
 
+4.
+#include<stdio.h>
+int main()
+{
+int b=65;
+void p = b;
+int c = (int)p;
+printf("%d\n", p);
+return 0;
+}
+
+Ans :  error- compile time
+/*Explanation: The void data type is not allowed because the size of the
+void data type is not known. It cannot be used as a generic data type.*/
+
+5.
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int *p = NULL;
+p = (int*)malloc(sizeof(int));
+*p = 10;
+free(p);
+int * q = (int*)malloc(sizeof(int));
+*q = 15;
+printf("%d, %d\n", *p, *q);
+return 0;
+}
+
+Ans:  15, 15
+  
+/*Explanation: The memory for malloc is allocated dynamically in heap
+area using ‘brk’ (keyword) system call. As we can see in figure (i) brk is
+pointing to the free (unallocated) pool of memory. When we call the
+malloc() function, the brk pointer allocates memory of 4B to ‘p’ and itself
+get shifted right, and (ii) also assigns a value 10 to the allocated space.
+Now we are doing free(p), (iii) the memory allocated by malloc get free
+and its content becomes zero or NULL, and brk return to its previous
+location. We can notice that ‘p’ is still pointing to the same address (figure
+iii). We again did malloc() to allocate memory for ‘q’ (figure iv). If we try
+to access the content of pointer ‘q’, it returns 15 as expected. Suppose if
+we access the content of pointer ‘p’, it also returns the same content as ‘q’.
+This was not expected from ‘p’ as we have already freed the ‘p’, but what
+actually happened is that we freed the memory from the heap area, but p
+still points to a memory location/address, as p is just a pointer variable,
+this ‘p’ is called dangling pointer.
+The dangling pointer in the program can be avoided by making p = NULL
+just after the free(p) statement*/
+
+
