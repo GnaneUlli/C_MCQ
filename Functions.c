84db5d80bb57b6adff1e51a346538faf675f4b75
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-10 08:15:41 +0530

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -81,4 +81,80 @@ The function runs infinetely because the there is a post-decrement operator is u
 
 Note: If we use pre-decrement operator like reverse(--n), then the output will be 5, 4, 3, 2, 1. Because before calling the function, it decrements the value of 'n'.*/
 
+4.
+#include<stdio.h>
+int sumdig(int);
+int main()
+{
+    int a, b;
+    a = sumdig(123);
+    b = sumdig(123);
+    printf("%d, %d\n", a, b);
+    return 0;
+}
+int sumdig(int n)
+{
+    int s, d;
+    if(n!=0)
+    {
+        d = n%10;
+        n = n/10;
+        s = d+sumdig(n);
+    }
+    else
+        return 0;
+    return s;
+}
+
+Ans :6,6
+    
+5.
+#include<stdio.h>
+
+int main()
+{
+    int fun(int);
+    int i = fun(10);
+    printf("%d\n", --i);
+    return 0;
+}
+int fun(int i)
+{
+   return (i++);
+}
+
+Ans :9
+/*Step 1: int fun(int); Here we declare the prototype of the function fun().
+
+Step 2: int i = fun(10); The variable i is declared as an integer type and the result of the fun(10) will be stored in the variable i.
+
+Step 3: int fun(int i){ return (i++); } Inside the fun() we are returning a value return(i++). It returns 10. because i++ is the post-increement operator.
+
+Step 4: Then the control back to the main function and the value 10 is assigned to variable i.
+
+Step 5: printf("%d\n", --i); Here --i denoted pre-increement. Hence it prints the value 9.*/
+    
+6.
+#include<stdio.h>
+int check (int, int);
+
+int main()
+{
+    int c;
+    c = check(10, 20);
+    printf("c=%d\n", c);
+    return 0;
+}
+int check(int i, int j)
+{
+    int *p, *q;
+    p=&i;
+    q=&j;
+    i>=45 ? return(*p): return(*q);
+}
+
+Ans : Compile error
+
+/* We cannot use return in ternary operators */
+
 
