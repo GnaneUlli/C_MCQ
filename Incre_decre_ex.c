238fb0e68c1f9ab4096f914e9052149ef7ef92b6
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-14 20:08:11 +0530

    Update Incre_decre_ex.c

--- Incre_decre_ex.c
+++ Incre_decre_ex.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 
 int main(){
+    
     int a=8;
+    
     int b=a++;
+    
     printf("%d\n",b);
+    
     int c = b*a;
+    
     printf("%d\n",c);
     
     c=++a;
+    
     printf("%d\n",a);
     
     printf("%d",c);
