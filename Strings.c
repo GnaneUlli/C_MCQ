abab5dcff3b4e0e7de70eaa702ee28b7bf3ed82f
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-16 07:52:14 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -33,3 +33,22 @@ Ans : error compile time
 /* so according to macro it takes char *a ,b.‘b’ is assigned to an address whereas ‘b’ is a character
 variable. In line 7, we are doing *(b+2), which is not a relevant operation
 with a character variable. Hence, compile throws an error */
+
+3.
+#include<stdio.h>
+int main()
+{
+char *s = "";
+do
+{
+printf("hello");
+}while(s);
+return 0;
+}
+
+Ans :  hello is printed infinite time
+  
+In a given program, ‘s’ is a character pointer that is pointing
+to an empty string, s contains the address of the empty string. In the while
+statement, the condition is applied on ‘s’ which contains the address that is
+always non-zero. The loop will execute infinite time.
