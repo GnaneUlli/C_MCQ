fdfe46acda18832054fb46976cd5aaacee79d8f7
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-23 09:28:07 +0530

    Create pointer_strings_const.c

---
+++ pointer_strings_const.c
@@ -0,0 +1,65 @@
+1.
+#include<stdio.h>
+int main()
+{
+char str[] = "India";
+const char *ptr;
+ptr = str;
+ptr++;
+//++*ptr;
+printf("%s", ptr);
+return 0;
+}
+Ans : ndia
+
+2.
+What will be the output if we uncomment the commented line in the
+above program?
+Ans : Error updating the constant expression is not allowed
+
+3.
+#include<stdio.h>
+int main()
+{
+char str[] = "India";
+char *const ptr;
+ptr = str;
+ptr++;
+printf("%s", ptr);
+return 0;
+}
+Ans :  Error updating the constant expression is not allowed
+
+4.
+#include<stdio.h>
+int main()
+{
+char str[] = "Hello";
+const char * const ptr;
+ptr = str; //line 3
+//ptr++; //line 4
+++*ptr; //line 5
+printf("%s", ptr);
+return 0;
+}
+Ans : Error at line 3 and 5
+
+5.
+What will be the output if we uncomment the commented line in the
+above program?
+Ans : Error at line 3,4 and 5.
+
+6.
+#include<stdio.h>
+int main()
+{
+char str[] = "India";
+char *ptr;
+ptr = str;
+ptr++;
+++*ptr;
+printf("%s", ptr);
+}
+
+Ans : odia
+
