bfd9da9602d9817cbd5da3c2cbf7116d4b29450b
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-06-29 08:39:19 +0530

    Update Incre_decre_ex.c

--- Incre_decre_ex.c
+++ Incre_decre_ex.c
@@ -1,3 +1,4 @@
+1.
 #include<stdio.h>
 int main(){
     
@@ -17,3 +18,58 @@ int main(){
     
     printf("%d",c);
 }
+
+Ans : Find answers on your own and check with gdb compiler
+
+2.
+#include<stdio.h>
+int main()
+{
+int a = 10;
+a++;
+(a++)++; //line 3
+printf("%d",a);
+return 0;
+}
+
+
+Ans  : Error l-value required
+/*We cannot assign a value to a number,
+there must be a variable at the left-hand side of the assignment operator for
+storing a value*/
+
+3.
+#include<stdio.h>
+int main()
+{
+int a = 1 , b = 0;
+int c = ++(++b)||a++;
+printf("%d %d %d",a,b,c);
+return 0;
+}
+
+Ans : Error l-value required
+
+/*At line 2, we are applying ++ operator
+on value not on variable, (++(++b)).*/
+
+4.
+#include<stdio.h>
+#define a 100
+int foo()
+{
+printf("%d ",a); //line 5
+return 0;
+}
+int main()
+{
+int b = a; //line 10
+#define a 200 //line 11
+printf("%d ",b);
+printf("%d ",a); //line 13
+foo();
+return 0;
+}
+
+Ans : 100,200,100
+//Here scope of once defined macro cannot be unchanged until its #undefine, but redeclaration variable changes variable value.
