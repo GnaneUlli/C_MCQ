973cdf4fc363bf8eaff90f614ef9683440918a46
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-07-13 15:03:50 +0530

    Update Float.c

--- Float.c
+++ Float.c
@@ -5,13 +5,9 @@ int main()
 {
     float a=0.7;
     if(a < 0.7)
-      
-        printf("C\n");
-  
+       printf("C\n");
     else
-      
-        printf("C++\n");
-  
+       printf("C++\n");
     return 0;
 }
 
